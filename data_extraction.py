b7fef2e3c0702d05c49b62537e3f02646ab3406e
Zainab-Gandomi <zeinabgandomi@yahoo.co.uk>
2023-11-06 14:54:32 +0000

    Create data_extraction.py file and add DataExtractor class

---
+++ data_extraction.py
@@ -0,0 +1,18 @@
+import csv             # for extract data from csv file
+import requests        # for extract data from AIP
+import boto3           # for extract data from AWS S3 
+
+class DataExtractor:
+
+    def __init__(self):
+        # Initialize any required configurations or connections here
+        pass
+
+    def extract_data_from_csv(self, file_path):
+        pass
+
+    def extract_data_from_api(self, url):
+        pass
+
+    def extract_data_from_s3_bucket(self, bucket_name, object_key):
+        pass
