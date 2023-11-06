6ede7435d0886b08dbce5b135c812034dfe090f9
Zainab-Gandomi <zeinabgandomi@yahoo.co.uk>
2023-11-06 14:50:04 +0000

    Create data_cleaning.py file and add DataCleaning class

---
+++ data_cleaning.py
@@ -0,0 +1,8 @@
+import pandas as pd  # appropriate libraries for data cleaning
+
+class DataCleaning:
+
+    def __init__(self):
+        # Initialize any necessary configuration or parameters for data cleaning here
+        pass
+
