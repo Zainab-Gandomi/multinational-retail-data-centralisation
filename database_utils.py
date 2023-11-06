86af62eb3f339dd4b7480b9a81199a083ba68952
Zainab-Gandomi <zeinabgandomi@yahoo.co.uk>
2023-11-06 15:56:22 +0000

    Create a method read_db_creds to read yaml file

--- database_utils.py
+++ database_utils.py
@@ -1,17 +1,13 @@
 import psycopg2  # the appropriate database library for database system
-
+import yaml
+from yaml.loader import SafeLoader
 
 class DatabaseConnector:
 
-    def __init__(self, host, database, user, password):
-        self.host = host
-        self.database = database
-        self.user = user
-        self.password = password
-        self.connection = None
+    def __init__(self,file_name = None):
+        self.file_name = file_name
 
-    def connect(self):
-        pass
-
-    def upload_data(self, data, table_name):
-        pass
+    def read_db_creds(self):
+        with open(self.file_name) as f:
+            data = yaml.load(f, Loader=SafeLoader)
+            return data 
