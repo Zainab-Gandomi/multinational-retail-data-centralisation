273ff9098053f58190b779c753ed49fda5ad6d45
Zainab-Gandomi <zeinabgandomi@yahoo.co.uk>
2023-11-06 16:02:34 +0000

    Create a method init_db_engine which read_db_creds and return an sqlalchemy database engine

--- database_utils.py
+++ database_utils.py
@@ -1,6 +1,8 @@
 import psycopg2  # the appropriate database library for database system
 import yaml
 from yaml.loader import SafeLoader
+from sqlalchemy import create_engine, inspect
+
 
 class DatabaseConnector:
 
@@ -11,3 +13,9 @@ class DatabaseConnector:
         with open(self.file_name) as f:
             data = yaml.load(f, Loader=SafeLoader)
             return data 
+        
+    def init_db_engine(self):
+        data_2 = self.read_db_creds()
+        engine = create_engine(f"postgresql+psycopg2://{data_2['RDS_USER']}:{data_2['RDS_PASSWORD']}@{data_2['RDS_HOST']}:{data_2['RDS_PORT']}/{data_2['RDS_DATABASE']}")
+        return engine
+
